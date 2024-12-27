7697e08009f7aad6476fcb0a34f95d146975dafe
shajibulhasn <shajibulhasan.work@gmail.com>
2024-12-27 22:36:36 +0600

    String Concatenation & interpolation

---
+++ String concatenation-and-interpolation.dart
@@ -0,0 +1,12 @@
+void main() {
+  String a = 'Shajibul';
+  String b = 'Hasan';
+  String c = 'Soaib';
+
+  print(a + b + c);
+
+  print('$a $b $c');
+
+  List l = ['Shajibul', 'Hasan', 'Soaib'];
+  print(l.join());
+}
